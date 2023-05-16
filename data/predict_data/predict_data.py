96d8e36f2a6200edab40070af1bec44d9f7a143c
Yan-Hao-Wang <bses30074@gmail.com>
2023-05-16 22:32:07 +0800

    add script and code in data folder

--- data/predict_data/predict_data.py
+++ data/predict_data/predict_data.py
@@ -1,8 +1,7 @@
 import pandas as pd
 import random
 
-df = pd.read_csv('03-11-5-percentclean-feature.csv')
-
+df = pd.read_csv('../output/03-11-five-percent-clean-feature.csv')
 # Get the number of rows in the DataFrame
 num_rows = len(df)
 
@@ -11,9 +10,10 @@ num_rows_to_extract = int(num_rows * 0.01)
 
 # Generate a random list of row indices to extract
 indices_to_extract = random.sample(range(num_rows), num_rows_to_extract)
+print(f"The predict data contain {len(indices_to_extract)} packets")
 
 # Extract the rows and create a new DataFrame
 df_extracted = df.iloc[indices_to_extract, :]
 
 # Write the extracted data to a new CSV file
-df_extracted.to_csv('predict_data.csv', index=False)
+df_extracted.to_csv('./predict_data.csv', index=False)
